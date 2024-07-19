a9b07a154445c3b77887248d61497599a8b8a29a
EstefaniaZhungo <113316595+EstefaniaZhungo@users.noreply.github.com>
2024-07-18 20:26:41 -0500

    Listo botones de registro admin

--- app/src/main/java/com/example/bibliotecaapp/AdminActivity.kt
+++ app/src/main/java/com/example/bibliotecaapp/AdminActivity.kt
@@ -1,11 +1,55 @@
 package com.example.bibliotecaapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class AdminActivity : AppCompatActivity() {
+    private lateinit var botonlibros: Button
+    private lateinit var botonautores: Button
+    private lateinit var botongenero: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+
+        botonlibros = findViewById(R.id.buttonLibros)
+        botonautores = findViewById(R.id.buttonAutores)
+        botongenero= findViewById(R.id.buttonGeneros)
+
+       // botonlibros.setOnClickListener{
+         //   showResult()
+        //}
+
+         botonautores.setOnClickListener{
+             showResultaurores()
+         }
+
+        botongenero.setOnClickListener{
+            showResultgenero()
+        }
+
+
+
+
+
     }
+    //Abrir autores
+    private fun showResultaurores() {
+        val intent = Intent(this, AgregarAutorActivity::class.java).apply {
+
+        }
+        startActivity(intent)
+    }
+
+//Abrir genero
+    private fun showResultgenero() {
+        val intent = Intent(this, AgregarGeneroActivity::class.java).apply {
+
+        }
+        startActivity(intent)
+    }
+
+
 }
--- app/src/main/java/com/example/bibliotecaapp/WelcomeActivity.kt
+++ app/src/main/java/com/example/bibliotecaapp/WelcomeActivity.kt
@@ -1,11 +1,18 @@
 package com.example.bibliotecaapp
 
+
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class WelcomeActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
     }
+
 }
